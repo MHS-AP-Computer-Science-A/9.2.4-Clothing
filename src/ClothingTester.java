b3c8a60df310317e9182499fa010d89ba1edce40
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-02-25 12:33:08 -0500

    Update ClothingTester.java

--- src/ClothingTester.java
+++ src/ClothingTester.java
@@ -2,7 +2,13 @@
 public class ClothingTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		TShirt ts = new TShirt("M", "Gray", "Cotton");
+		Sweatshirt ss = new Sweatshirt("L", "Gold", true);
+		Jeans j = new Jeans("S");
+		
+		System.out.println("T-Shirt: " + ts.getSize() + " " + ts.getColor() + " " + ts.getFabric());
+		System.out.println("Sweatshirt: " + ss.getSize() + " " + ss.getColor() + " Has hood: " + ss.hasHood());
+		System.out.println("Jeans: " + j.getSize() + " " + j.getColor());
 
 	}
 
